972bc648915dbe5761594ce38fea9395c1fae179
Mamas Nicolaou <mamas.nicolaou@yahoo.com>
2024-03-11 23:12:41 +0200

    part 1 cleanup (#2)
    * first draft
    
    * part 1 up to 1.11 completed
    
    * part 1 completed
    
    * cleanup and fix for unicafe
    
    * more cleanup
    
    ---------
    
    Co-authored-by: Mamas Nicolaou <mnicolaou@vubiquity.net>

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -18,13 +18,14 @@ const Statistics = ({bad, good, neutral, all}) => {
     }else{
         return(
             <table>
+                <tbody>
                     <StatisticLine text={"good"} value={good}/>
                     <StatisticLine text={"neutral"} value={neutral}/>
                     <StatisticLine text={"bad"} value={bad}/>
                     <StatisticLine text={"all"} value={all}/>
                     <StatisticLine text={"average"} value={(good+bad*-1)/all}/>
                     <StatisticLine text={"positive"} value={(good/all*100)+"%"}/>
-
+                </tbody>
             </table>);
     }
 
