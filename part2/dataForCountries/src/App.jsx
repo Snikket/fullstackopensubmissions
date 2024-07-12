78b96ee33d5030a8e3024289464e39aa9ad9c9ed
Snikket <mamas.nicolaou@yahoo.com>
2024-07-11 21:20:10 -0400

    exercise 2.20 completed

--- part2/dataForCountries/src/App.jsx
+++ part2/dataForCountries/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import countriesService from "./services/countriesService";
+import weatherService from "./services/weatherService";
 import Countries from "./components/Countries";
 import Notification from "./components/Notification";
 import Country from "./components/Country";
+
 function App() {
   const [filterTerm, setFilterTerm] = useState("");
   const [countries, setCountries] = useState([]);
+  const [temp, setTemp] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+
+  useEffect(() => {
+    if (!selectedCity) {
+      return;
+    }
+    console.log('selected city is',selectedCity)
+    weatherService.getByCity({ cityName: selectedCity })
+      .then(data => {
+        setTemp(data.main.temp);
+        setWind(data.wind.speed)
+        setWeatherIcon(data.weather[0].icon)
+      })
+  },[selectedCity]);
 
   useEffect(() => {
     countriesService.getAll().then((initialCountries) => {
@@ -14,16 +34,24 @@ function App() {
     });
   }, []);
 
+  const countriesToShow =
+  filterTerm === ""
+    ? countries
+    : countries.filter((country) =>
+        country.name.common.toLowerCase().includes(filterTerm.toLowerCase())
+      );
+
+  useEffect(() => {
+    if (countriesToShow.length === 1) {
+      setSelectedCity(countriesToShow[0].capital[0]);
+    }
+  }, [filterTerm, countriesToShow]);
+
   const handleSearchTermChange = (event) => {
     setFilterTerm(event.target.value);
   };
 
-  const countriesToShow =
-    filterTerm === ""
-      ? countries
-      : countries.filter((country) =>
-          country.name.common.toLowerCase().includes(filterTerm.toLowerCase())
-        );
+
 
   return (
     <div>
@@ -37,7 +65,7 @@ function App() {
       ) : null}
 
       {countriesToShow.length === 1 ? (
-        <Country country={countriesToShow[0]}/> 
+        <Country country={countriesToShow[0]} temperature={temp} wind={wind} icon={weatherIcon}/> 
       ) : null}
     </div>
   );
--- part2/dataForCountries/src/components/Country.jsx
+++ part2/dataForCountries/src/components/Country.jsx
@@ -1,11 +1,24 @@
-const Country = ({country}) => {
-    console.log("languages:",country.languages)
+import weatherService from '../services/weatherService';
+
+const Country = ({country, temperature, wind, icon}) => {
+    console.log("Country params! country:",country)
+    console.log("Country params! temperature:",temperature)
+    console.log("Country params! wind:",wind)
+    console.log("Country params! icon:",icon)
+    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
+    console.log(iconUrl)
+
     return <div>
         <h2>{country.name.common}</h2>
         <p>capital {country.capital}</p>
         <p>area {country.area}</p>
+        <h3>languages:</h3>
         <Languages languages={country.languages}/>
         <img alt={country.flags['alt']} src={country.flags['png']}/>
+        <h3>Weather in {country.capital}</h3>
+        <p>temperature {temperature} Celcius</p>
+        <img src={iconUrl}/>
+        <p>wind {wind}m/s</p>
     </div>
 }
 
